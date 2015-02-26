1cb2aacc501c1ae9fdb6834c0a15ac7f7be5685d
Stefano Teso <stefano.teso@gmail.com>
2015-02-26 15:52:28 +0100

    kernels: check that there are at least two entities.

--- ocelot/kernels/base.py
+++ ocelot/kernels/base.py
@@ -11,6 +11,8 @@ class _Kernel(object):
     """
     def __init__(self, entities, **kwargs):
         self._entities = entities
+        if not (len(self._entities) >= 2):
+            raise ValueError("kernels need at least two entities.")
         self._do_normalize = kwargs.get("do_normalize", True)
         self._matrix = None
     def __len__(self):
