658243afe18a9a2ea610cf3413661998c64dde3b
Stefano Teso <stefano.teso@gmail.com>
2015-03-02 12:57:15 +0100

    kernels/graph: document the DiffusionKernel.

--- ocelot/kernels/graph.py
+++ ocelot/kernels/graph.py
@@ -5,16 +5,29 @@ import numpy as np
 from .base import _Kernel
 
 class DiffusionKernel(_Kernel):
-    """The diffusion kernel between graph nodes [Kondor02]_."""
+    """The diffusion kernel between graph nodes [Kondor02]_.
+
+    The diffusion kernel between two nodes of a graph :math:`G` is defined as:
+
+    .. math::
+
+        K(u,u') := (\\exp ( -\\beta H \\ ))_{u,u'}
+
+    where :math:`H` is the unnormalized Laplacian matrix of :math:`G`, and
+    :math:`\\beta` is a user-provided parameter.
+
+    :param adjmatrix: the adjacency matrix of :math:`G`.
+    :param beta: a non-negative smoothing parameter.
+    """
     def __init__(self, adjmatrix, beta = 1.0, **kwargs):
         n, m = adjmatrix.shape
         assert n == m
         self._adjmatrix = adjmatrix
         self._beta = beta
         super(DiffusionKernel, self).__init__(range(n), **kwargs)
+
     def _compute_all(self):
         import scipy.linalg as la
-        # XXX this does not work for weighted adjacency matrices
         a = self._adjmatrix
         d = np.identity(a.shape[0]) * np.sum(a, axis = 1)
         e = la.expm(self._beta * (a - d))
