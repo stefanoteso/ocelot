b22276bfd6662eced5e4932ee19fa69e177dce42
Stefano Teso <stefano.teso@gmail.com>
2015-03-03 14:08:24 +0100

    docs: enable todo lists by default.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -265,3 +265,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# -- More options ---------------------------------------------------------
+
+todo_include_todos = True
