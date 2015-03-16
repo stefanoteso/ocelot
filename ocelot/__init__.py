b416a85738b294589c2da944f91b34bff2d5ab0d
Stefano Teso <stefano.teso@gmail.com>
2015-03-16 22:53:40 +0100

    ocelot: add ocelot.config.

--- ocelot/__init__.py
+++ ocelot/__init__.py
@@ -6,3 +6,30 @@ from go import *
 from experiments import *
 
 __version__ = "0.0-dontuse"
+
+import configparser
+
+_DEFAULT_INI = """\
+[virtuoso]
+ini = /home/virtuoso/virtuoso.ini
+virtuoso = virtuoso-t
+isql = isql
+endpoint = http://localhost:8890/sparql
+default_graph = http://ocelot
+"""
+
+# XXX add support for the xdg config path
+_INI_PATHS = ["ocelot.ini"]
+
+config = configparser.ConfigParser()
+for path in _INI_PATHS:
+    config.read(path)
+    if len(config.sections()) > 0:
+        print "Loaded config from '{}'".format(path)
+        break
+
+if len(config.sections()) == 0:
+    print "Writing default config to '{}'".format(path)
+    with open(_INI_PATHS[0], "wb") as fp:
+        fp.writelines(_DEFAULT_INI)
+    config.read(_INI_PATHS[0])
