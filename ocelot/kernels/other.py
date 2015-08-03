a30d1a3514ead00336de45423da320709ce5f389
Stefano Teso <stefano.teso@gmail.com>
2015-08-03 15:06:57 +0200

    kernels/other: improve and test the pairwise kernel.

--- ocelot/kernels/other.py
+++ ocelot/kernels/other.py
@@ -1,6 +1,7 @@
 # -*- coding: utf8 -*-
 
 import numpy as np
+import itertools as it
 
 from . import Kernel, DummyKernel
 
@@ -32,6 +33,9 @@ class PairwiseKernel(Kernel):
     .. math::
         k[(i,j),(n,m)] := k[i,n] + k[j,m] + k[i,m] + k[j,n]
 
+    The pairwise kernel matrix will have the same ``dtype`` as the Gram matrix
+    of the given subkernel.
+
     :param indices: indices of the elements to combine.
     :param subkernel: a `Kernel` instance or an ``numpy.ndarray``.
     :param op: either ``"product"`` or ``"sum"``.
@@ -47,14 +51,44 @@ class PairwiseKernel(Kernel):
             raise ValueError, "invalid op '{}'".format(op)
 
     def _compute_all(self):
+        indices = list(enumerate(self._entities))
+        assert len(indices) == len(set(indices)), "repeated indices found"
         try:
             submatrix = self._subkernel.compute()
         except AttributeError, e:
             submatrix = self._subkernel
-        matrix = np.zeros((len(self), len(self)), dtype=np.float32)
-        for out_i, (i, j) in enumerate(self._entities): # pairs
-            for out_j, (n, m) in enumerate(self._entities): # pairs
+        matrix = np.zeros((len(self), len(self)), dtype=submatrix.dtype)
+        for out_i, (i, j) in indices:
+            for out_j, (n, m) in indices:
                 kin, kim = submatrix[i,n], submatrix[i,m]
                 kjn, kjm = submatrix[j,n], submatrix[j,m]
                 matrix[out_i, out_j] = self.op(kin, kjm, kim, kjn)
         return matrix
+
+class _TestPairwiseKernel(object):
+    _SUBMATRIX = np.arange(4).reshape(2, 2).astype(np.float32)
+    _INDICES = list(it.product(range(2), range(2)))
+
+    def _test_results(self, expected, *args, **kwargs):
+        kwargs["do_normalize"] = False
+        kernel = PairwiseKernel(*args, **kwargs)
+        output = kernel.compute()
+        assert (output == expected).all()
+
+    def test_tensor_product(self):
+        EXPECTED = np.array([
+            [0,  0,  0,  2],
+            [0,  2,  2,  6],
+            [0,  2,  2,  6],
+            [8, 12, 12, 18],
+        ], dtype=np.float32)
+        self._test_results(EXPECTED, self._INDICES, self._SUBMATRIX, op="product")
+
+    def test_tensor_sum(self):
+        EXPECTED = np.array([
+            [0,  2,  2,  4],
+            [4,  6,  6,  8],
+            [4,  6,  6,  8],
+            [8, 10, 10, 12],
+        ], dtype=np.float32)
+        self._test_results(EXPECTED, self._INDICES, self._SUBMATRIX, op="sum")
