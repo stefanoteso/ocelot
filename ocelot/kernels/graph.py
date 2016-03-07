561519a6c3bd09078a6092bfd6c5d856114f1aac
Stefano Teso <stefano.teso@gmail.com>
2016-03-07 14:48:32 +0100

    ocelot/kernels: clean-up the DiffusionKernel.

--- ocelot/kernels/graph.py
+++ ocelot/kernels/graph.py
@@ -1,41 +1,51 @@
 # -*- coding: utf8 -*-
 
 import numpy as np
+import scipy.linalg as la
 
 from . import Kernel
 
 class DiffusionKernel(Kernel):
     """The diffusion kernel between graph nodes [Kondor02]_.
 
-    The diffusion kernel between two nodes of a graph :math:`G` is defined as:
+    The diffusion kernel between two nodes of a graph :math:`G`, defined by:
 
     .. math::
 
         K(u,u') := (\\exp ( -\\beta H \\ ))_{u,u'}
 
-    where :math:`H` is the unnormalized Laplacian matrix of :math:`G`, and
-    :math:`\\beta` is a user-provided parameter.
+    Here :math:`H` is the unnormalized Laplacian matrix of :math:`G`, and
+    :math:`\\beta` is a user-provided smoothing parameter.
 
-    :param adjmatrix: the adjacency matrix of :math:`G`.
-    :param beta: a non-negative smoothing parameter.
+    Parameters
+    ----------
+    A : numpy.ndarray
+        Adjacency matrix of the graph.
+    beta : float, optional
+        Non-negative smoothing parameter, defaults to 1.0.
+
+    References
+    ----------
+    .. [1] Kondor and Lafferty, *Diffusion Kernels on Graphs and Other Discrete
+           Input Spaces*, 2002.
     """
-    def __init__(self, adjmatrix, beta = 1.0, **kwargs):
-        n, m = adjmatrix.shape
-        assert n == m
-        self._adjmatrix = adjmatrix
-        self._beta = beta
-        super(DiffusionKernel, self).__init__(range(n), **kwargs)
+    def __init__(self, A, beta=1.0, **kwargs):
+        if A.ndim != 2:
+            raise ValueError("A is not a matrix")
+        if A.shape[0] != A.shape[1]:
+            raise ValueError("A is not a square matrix")
+        super(DiffusionKernel, self).__init__([0] * A.shape[0], **kwargs)
+        self._A, self._beta = A.astype(self.dtype), beta
 
     def _compute_all(self):
-        import scipy.linalg as la
-        a = self._adjmatrix
-        d = np.identity(a.shape[0]) * np.sum(a, axis = 1)
-        e = la.expm(self._beta * (a - d))
+        A = self._A
+        D = np.eye(A.shape[0], dtype=self.dtype) * np.sum(A, axis=1)
+        E = la.expm(self._beta * (A - D))
 
-        # being approximate, matrix exponentiation can give slightly asymmetric
-        # results; here we fix them if they are small enough.
-        asymmetry = np.linalg.norm(e.T - e, ord = "fro")
+        # Matrix exponentiation can give slightly asymmetric results; here we
+        # fix them if they are small enough.
+        asymmetry = np.linalg.norm(E.T - E, ord="fro")
         if asymmetry > 0.0:
             assert asymmetry < 1e-10, "the gods are playful today"
-            e = 0.5 * (e.T + e)
-        return e
+            E = 0.5 * (E.T + E)
+        return E
