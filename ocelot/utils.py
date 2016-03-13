eec8aebc4314065b39ed33938942ddd27b60a51d
Stefano Teso <stefano.teso@gmail.com>
2016-03-13 11:51:17 +0100

    ocelot/utils: add split_tr_ts().

--- ocelot/utils.py
+++ ocelot/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 from sklearn.utils import check_random_state
 
 def permute(l, rng=None):
@@ -7,3 +8,12 @@ def permute(l, rng=None):
 
     perm = list(rng.permutation(len(l)))
     return [l[perm[i]] for i in range(len(l))]
+
+def split_tr_ts(array, indices0, indices1):
+    if array.ndim == 1:
+        return array[indices0], array[indices1]
+    elif array.ndim == 2:
+        return array[np.ix_(tr_indices, tr_indices)], \
+               array[np.ix_(ts_indices, tr_indices)]
+    else:
+        raise ValueError("invalid ndim")
