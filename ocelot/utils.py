e7eafd643eb57d20a06d59eff1cb519146218212
Stefano Teso <stefano.teso@gmail.com>
2016-03-15 17:57:08 +0100

    ocelot/utils: add a few utilites.

--- ocelot/utils.py
+++ ocelot/utils.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from os.path import isdir, abspath
 from sklearn.utils import check_random_state
+
+def checkdir(path):
+    return path and isdir(abspath(path))
+
+def validate(valid, values):
+    if values is None:
+        values = valid
+    else:
+        for value in values:
+            if not value in valid:
+                raise ValueError("invalid value '{}'".format(value))
+    return values
 
 def permute(l, rng=None):
     rng = check_random_state(rng)
 
     perm = list(rng.permutation(len(l)))
     return [l[perm[i]] for i in range(len(l))]
+
+def powerset(iterable):
+    l = list(iterable)
+    return it.chain(from_iterable(it.combinations(l, r) for r in xrange(len(l) + 1)))
 
 def split_tr_ts(array, indices0, indices1):
     if array.ndim == 1:
