ocelot/kernels: fix normalization of string kernels.
The sparse addition in the recursive string kernel (see _add_survivors) for
some reason changes the type of self._matrix from np.ndarray to np.matrix;
this breaks normalize() because the matrix.diag() behaves differently for
arrays and matrices.

ocelot/kernels/__init__.py
@@ -56,7 +56,7 @@ class Kernel(object):
         Relies on `self._compute_all`.
         """
         if self._matrix is None:
-            self._matrix = self._compute_all()
+            self._matrix = np.array(self._compute_all())
             if self._do_normalize:
                 self._matrix = self._normalize(self._matrix)
         return self._matrix