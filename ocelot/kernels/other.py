67748509185fd1cd459500d4fa64ec2a2056e5ac
Stefano Teso <stefano.teso@gmail.com>
2016-03-30 10:30:09 +0200

    ocelot/kernels/other: fixup, simplify, test.

--- ocelot/kernels/other.py
+++ ocelot/kernels/other.py
@@ -1,12 +1,12 @@
 # -*- coding: utf8 -*-
 
 import numpy as np
-import itertools as it
+from itertools import product
 
-from . import Kernel, DummyKernel
+from ocelot.kernels import Kernel
 
-class EmpiricalFeatures(object):
-    """The Empirical Kernel Map [Scholkopf99]_.
+class EmpiricalKernel(Kernel):
+    """Empirical kernel map over an underlying kernel [1]_.
 
     Given a fixed set of patterns :math:`(z_1, \\ldots, z_m)` and an input
     pattern :math:`x`, the empirical kernel map is defined as:
@@ -17,33 +17,35 @@ class EmpiricalFeatures(object):
 
     for any given sub-kernel :math:`k`.
 
-    NOTE: patterns and indices refer to elements in the sub-kernel.
+    Parameters
+    ----------
+    indices : ordered list
+        Indices of anchor patterns in the subkernel.
+    subkernel : np.ndarray of shape (n, n) or Kernel
+        The subkernel.
+
+    References
+    ----------
+    .. [1] Scholkopf et al., "Input Space Versus Feature Space in Kernel-Based
+           Methods", 1999.
     """
-    def __init__(self, subkernel, patterns):
-        self.subkernel = subkernel
-        self.patterns = patterns
-    def compute(self, indices):
-        return self.subkernel.compute()[indices, self.patterns]
+    def __init__(self, indices, subkernel, **kwargs):
+        super(EmpiricalKernel, self).__init__(indices, **kwargs)
+        self._subkernel = subkernel
 
-class EmpiricalKernelMap(Kernel):
-    """Empirical kernel map over an underlying kernel[1].
-
-    *References*
-
-    [1] Scholkopf et al., "Input Space Versus Feature Space in Kernel-Based
-        Methods", 1999.
-    """
-    def __init__(self, *args, **kwargs):
-        import ocelot.features
-        super(EmpiricalKernelMap, self).__init__(*args, **kwargs)
-        self.features = features.EmpiricalFeatures(args[0], range(len(self)))
     def _compute_all(self):
-        LinearKernel(self.features, range(len(self)))
+        indices = self._entities
+
+        try:
+            submatrix = self._subkernel.compute()
+        except AttributeError:
+            submatrix = self._subkernel
+
+        phi = submatrix[indices].astype(self.dtype)
+        return np.dot(phi, phi.T)
 
 class PairwiseKernel(Kernel):
-    """The pairwise kernel.
-
-    It computes a kernel between pairs of elements as follows:
+    """The pairwise kernel, defined as:
 
     .. math::
         k[(i,j),(n,m)] := k[i,n]k[j,m] + k[i,m]k[j,n]
@@ -56,18 +58,18 @@ class PairwiseKernel(Kernel):
     The pairwise kernel matrix will have the same ``dtype`` as the Gram matrix
     of the given subkernel.
 
-    .. warning::
+    Warning: may be affected by `https://github.com/numpy/numpy/issues/2396`.
 
-        Make sure not to be affected by `https://github.com/numpy/numpy/issues/2396`.
-
-    .. todo::
-
-        Implement block-by-block computation.
-
-    :param indices: indices of the elements to combine.
-    :param subkernel: a `Kernel` instance or an ``numpy.ndarray``.
-    :param op: either ``"product"`` or ``"sum"`` (default: ``"product"``).
+    Parameters
+    ----------
+    pairs : ordered collection
+        Pairs of indices of the elements to combine.
+    subkernel : np.ndarray of shape (n, n) or Kernel instance
+        Sub-kernel
+    op : str, optional. (defaults to "product")
+        Either "product" or "sum".
     """
+    # TODO implement block-wise computation
     def __init__(self, pairs, subkernel, op="product", **kwargs):
         super(PairwiseKernel, self).__init__(pairs, **kwargs)
         self._subkernel = subkernel
@@ -91,42 +93,60 @@ class PairwiseKernel(Kernel):
 
     def _compute_all(self):
         indices = list(enumerate(self._entities))
-        assert len(indices) == len(set(indices)), "repeated indices found"
         try:
             submatrix = self._subkernel.compute()
         except AttributeError, e:
             submatrix = self._subkernel
+
         matrix = np.zeros((len(self), len(self)), dtype=submatrix.dtype)
         if self._op == "product":
             self._compute_range_product(matrix, submatrix, indices, indices)
         else:
             self._compute_range_sum(matrix, submatrix, indices, indices)
+
         return matrix
+
+class _TestEmpiricalKernel(object):
+    _SUBMATRIX = np.array([
+        [1.0, 0.5, 0.0, 0.0],
+        [0.5, 1.0, 0.5, 0.0],
+        [0.0, 0.5, 1.0, 0.5],
+        [0.0, 0.0, 0.5, 1.0],
+    ])
+
+    def test_compute(self):
+        INDICES = [1, 2]
+        EXPECTED = np.array([[1.5, 1.0], [1.0, 1.5]])
+
+        kernel = EmpiricalKernel(INDICES, self._SUBMATRIX, normalize=False)
+        output = kernel.compute()
+        assert (output == EXPECTED).all()
 
 class _TestPairwiseKernel(object):
     _SUBMATRIX = np.arange(4).reshape(2, 2).astype(np.float32)
-    _INDICES = list(it.product(range(2), range(2)))
+    _INDICES = list(product(range(2), range(2)))
 
     def _test_results(self, expected, *args, **kwargs):
-        kwargs["do_normalize"] = False
         kernel = PairwiseKernel(*args, **kwargs)
         output = kernel.compute()
         assert (output == expected).all()
 
-    def test_tensor_product(self):
+    def test_product(self):
         EXPECTED = np.array([
             [0,  0,  0,  2],
             [0,  2,  2,  6],
             [0,  2,  2,  6],
             [8, 12, 12, 18],
         ], dtype=np.float32)
-        self._test_results(EXPECTED, self._INDICES, self._SUBMATRIX, op="product")
+        self._test_results(EXPECTED, self._INDICES, self._SUBMATRIX,
+                           op="product", normalize=False)
 
-    def test_tensor_sum(self):
+    def test_sum(self):
         EXPECTED = np.array([
             [0,  2,  2,  4],
             [4,  6,  6,  8],
             [4,  6,  6,  8],
             [8, 10, 10, 12],
         ], dtype=np.float32)
-        self._test_results(EXPECTED, self._INDICES, self._SUBMATRIX, op="sum")
+        self._test_results(EXPECTED, self._INDICES, self._SUBMATRIX,
+                           op="sum", normalize=False)
