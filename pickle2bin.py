556c073df186ca62b3e9fd89ad2fda179858d052
Stefano Teso <stefano.teso@gmail.com>
2016-04-14 15:23:08 +0200

    pickle2bin: add script.

---
+++ pickle2bin.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import pickle
+import numpy as np
+
+if len(sys.argv) != 3:
+    print("Usage: {} <pickle input> <binary output>".format(sys.argv[0]))
+    sys.exit(1)
+
+print("loading...")
+with open(sys.argv[1], "rb") as fp:
+    # XXX the encoding thing works around python 2/3 incompatibility, see:
+    #
+    #   http://stackoverflow.com/questions/11305790/pickle-incompatability-of-numpy-arrays-between-python-2-and-3
+    #
+    try:
+        matrix = pickle.load(fp)
+    except:
+        matrix = pickle.load(fp, encoding='latin1')
+
+print("storing...")
+with open(sys.argv[2], "wb") as fp:
+    info = np.array(matrix.shape)
+    info.astype(np.int32).tofile(fp)
+    matrix.astype(np.float32).tofile(fp)
